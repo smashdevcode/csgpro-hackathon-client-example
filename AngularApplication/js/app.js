893d87312259653fabdb8c6763a2faf3627847fe
James Churchill <james@smashdev.com>
2014-09-11 20:56:54 -0700

    First pass at the Projects and Add/Edit Project views

--- AngularApplication/js/app.js
+++ AngularApplication/js/app.js
@@ -1,6 +1,3 @@
-/**
- * Created by James on 8/8/2014.
- */
 
 angular.module('api', [])
 
@@ -14,38 +11,91 @@ angular.module('api', [])
 
 angular.module('app', ['ngRoute','api'])
 
-    .value('apiURL', 'https://csgprohackathonapi.azurewebsites.net/api/')
+    .value('apiURL', 'http://localhost:57214/api/')
+    //.value('apiURL', 'https://csgprohackathonapi.azurewebsites.net/api/')
 
-    .factory('API', function($http, apiURL) {
+    .factory('auth', function () {
         return new function () {
             var self = this;
 
-            self.getUser = function (username, password, success) {
+            self.isAuthenticated = false;
+            self.username = null;
+            self.password = null;
 
-                var authString = btoa(username + ':' + password);
+            self.login = function (username, password) {
+
+                // JCTODO need to authenticate the user
+
+                self.isAuthenticated = true;
+                self.username = username;
+                self.password = password;
+            };
+
+            self.logout = function () {
+                self.isAuthenticated = false;
+                self.username = null;
+                self.password = null;
+            };
+        };
+    })
+
+    .factory('API', function($http, apiURL, auth) {
+        return new function () {
+            var self = this;
+
+            // JCTODO need to check within each function if the user is logged
+
+            self.getUser = function (success) {
+                self.http('GET', 'users', null, success);
+            };
+
+            self.getProjects = function (success) {
+                self.http('GET', 'projects', null, success);
+            };
+
+            self.getProject = function (projectId, success) {
+                self.http('GET', 'projects/' + projectId, null, success);
+            };
+
+            self.addProject = function (project, success) {
+                self.http('POST', 'projects', project, success);
+            };
+
+            self.updateProject = function (project, success) {
+                self.http('PUT', 'projects/' + project.ProjectId, project, success);
+            };
+
+            self.http = function (method, action, data, success) {
+
+                var authString = btoa(auth.username + ':' + auth.password);
 
                 $http({
-                    headers: {
-                        'Authorization': 'Basic ' + authString
-                    },
-                    method: 'GET',
-                    url: apiURL + 'users'
+                        headers: {
+                            'Authorization': 'Basic ' + authString
+                        },
+                        method: method,
+                        url: apiURL + action,
+                        data: data
                     }).
-                    success(function(data, status, headers, config) {
+                    success(function (data, status, headers, config) {
                         // this callback will be called asynchronously
                         // when the response is available
                         console.log('success');
+
                         if (success) {
                             success(data);
                         }
                     }).
-                    error(function(data, status, headers, config) {
+                    error(function (data, status, headers, config) {
                         // called asynchronously if an error occurs
                         // or server returns response with an error status.
                         console.log(status);
+
+                        // JCTODO display error message???
                     });
 
             };
+
         };
     })
 
@@ -62,6 +112,14 @@ angular.module('app', ['ngRoute','api'])
             .when('/projects', {
                 controller:'Projects',
                 templateUrl:'views/projects.html'
+            })
+            .when('/projects/add', {
+                controller: 'ProjectAdd',
+                templateUrl: 'views/project-add.html'
+            })
+            .when('/projects/:projectId', {
+                controller: 'ProjectEdit',
+                templateUrl: 'views/project-edit.html'
             })
             .when('/register', {
                 controller:'Register',
@@ -80,8 +138,14 @@ angular.module('app', ['ngRoute','api'])
             });
     })
 
-    .controller('Home', function($scope, API) {
-        var user = API.getUser('jennyc', 'password', function (user) {
+    .controller('Home', function ($scope, API, auth) {
+
+        // JCTODO need to provide a way for the user to login
+        if (!auth.isAuthenticated) {
+            auth.login('jennyc', 'password');
+        }
+
+        API.getUser(function (user) {
             console.log(user);
 
             if (user) {
@@ -90,7 +154,66 @@ angular.module('app', ['ngRoute','api'])
         });
     })
 
-    .controller('Projects', function($scope) {
+    .controller('Projects', function ($scope, $location, API, auth) {
+        var self = this;
+
+        // JCTODO need to provide a way for the user to login
+        if (!auth.isAuthenticated) {
+            auth.login('jennyc', 'password');
+        }
+
+        API.getProjects(function (projects) {
+            $scope.projects = projects;
+        });
+
+        $scope.add = function () {
+            $location.path('/projects/add');
+        };
+
+        $scope.edit = function (projectId) {
+            $location.path('/projects/' + projectId);
+        };
+    })
+
+    .controller('ProjectAdd', function ($scope, $location, API, auth) {
+        var self = this;
+
+        // JCTODO need to provide a way for the user to login
+        if (!auth.isAuthenticated) {
+            auth.login('jennyc', 'password');
+        }
+
+        $scope.project = {};
+
+        $scope.save = function () {
+            // JCTODO need to handle errors
+
+            API.addProject($scope.project, function () {
+                $location.path('/projects');
+            });
+        };
+    })
+
+    .controller('ProjectEdit', function ($scope, $location, $routeParams, API, auth) {
+        var self = this;
+
+        // JCTODO need to provide a way for the user to login
+        if (!auth.isAuthenticated) {
+            auth.login('jennyc', 'password');
+        }
+
+        API.getProject($routeParams.projectId, function (project) {
+            $scope.project = project;
+        });
+
+        $scope.save = function () {
+
+            // JCTODO need to handle errors
+
+            API.updateProject($scope.project, function () {
+                $location.path('/projects');
+            });
+        };
     })
 
     .controller('Register', function($scope) {
