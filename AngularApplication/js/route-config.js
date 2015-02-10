1a99ae74af97168a1cd601a329d1a7bf86c79b99
James Churchill <james@smashdev.com>
2015-02-10 07:58:17 -0800

    Added missing IIFE

--- AngularApplication/js/route-config.js
+++ AngularApplication/js/route-config.js
@@ -1,59 +1,63 @@
 
-angular
-    .module('app')
-    .config(config);
+(function() {
+    'use strict';
 
-function config($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: 'Home',
-            controllerAs: 'vm',
-            templateUrl: 'views/home.html'
-        })
-        .when('/home', {
-            controller: 'Home',
-            controllerAs: 'vm',
-            templateUrl: 'views/home.html'
-        })
-        .when('/projects', {
-            controller: 'Projects',
-            controllerAs: 'vm',
-            templateUrl: 'views/projects.html'
-            //resolve: {
-            //    temp1: function ($q, $timeout) {
-            //        var defer = $q.defer();
+    angular
+        .module('app')
+        .config(config);
 
-            //        $timeout(function () {
-            //            defer.resolve();
-            //        }, 2000);
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'Home',
+                controllerAs: 'vm',
+                templateUrl: 'views/home.html'
+            })
+            .when('/home', {
+                controller: 'Home',
+                controllerAs: 'vm',
+                templateUrl: 'views/home.html'
+            })
+            .when('/projects', {
+                controller: 'Projects',
+                controllerAs: 'vm',
+                templateUrl: 'views/projects.html'
+                //resolve: {
+                //    temp1: function ($q, $timeout) {
+                //        var defer = $q.defer();
 
-            //        return defer.promise;
-            //    },
-            //    temp2: function ($q) {
-            //        var defer = $q.defer();
+                //        $timeout(function () {
+                //            defer.resolve();
+                //        }, 2000);
 
-            //        defer.reject();
+                //        return defer.promise;
+                //    },
+                //    temp2: function ($q) {
+                //        var defer = $q.defer();
 
-            //        return defer.promise;
-            //    }
-            //}
-        })
-        .when('/projects/add', {
-            controller: 'ProjectAdd',
-            controllerAs: 'vm',
-            templateUrl: 'views/project-add.html'
-        })
-        .when('/projects/:projectId', {
-            controller: 'ProjectEdit',
-            controllerAs: 'vm',
-            templateUrl: 'views/project-edit.html'
-        })
-        .when('/register', {
-            controller: 'Register',
-            controllerAs: 'vm',
-            templateUrl: 'views/register.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-}
+                //        defer.reject();
+
+                //        return defer.promise;
+                //    }
+                //}
+            })
+            .when('/projects/add', {
+                controller: 'ProjectAdd',
+                controllerAs: 'vm',
+                templateUrl: 'views/project-add.html'
+            })
+            .when('/projects/:projectId', {
+                controller: 'ProjectEdit',
+                controllerAs: 'vm',
+                templateUrl: 'views/project-edit.html'
+            })
+            .when('/register', {
+                controller: 'Register',
+                controllerAs: 'vm',
+                templateUrl: 'views/register.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+})();
